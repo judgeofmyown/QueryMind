cd4e12050b6096eb513fd7db71436384ce386926
judgeofmyown <tanbir100000@gmail.com>
2025-03-21 22:36:42 +0530

    initial commit

---
+++ querymind/app/dashboard/layout.js
@@ -0,0 +1,10 @@
+import "../globals.css"
+export default function dashlayout({children}){
+    return(
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>    
+    )
+}
---
+++ querymind/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline">landing page</h2>
+    </div>
+  );
+}
